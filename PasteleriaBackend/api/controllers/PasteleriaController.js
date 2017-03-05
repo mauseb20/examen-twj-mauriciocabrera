cb0ba34b5a64006bdcefaf2ebe3fcaa10ca666f7
Mauricio Cabrera <mauseb91@gmail.com>
2017-03-05 12:48:17 -0500

    Modelo

---
+++ PasteleriaBackend/api/controllers/PasteleriaController.js
@@ -0,0 +1,11 @@
+/**
+ * PasteleriaController
+ *
+ * @description :: Server-side logic for managing pastelerias
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
