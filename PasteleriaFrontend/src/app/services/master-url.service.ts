8e82373cfff8e020ac47b862eaf214f24c4eeb73
Mauricio Cabrera <mauseb91@gmail.com>
2017-03-07 13:33:10 -0500

    comit muestra

--- PasteleriaFrontend/src/app/services/master-url.service.ts
+++ PasteleriaFrontend/src/app/services/master-url.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MasterUrlService {
-
   private _url:string;
   constructor() {
-    this._url="http://localhost:1337/";
-    //this._url="https://examen-twj-mauriciocabrera-mauseb20.c9users.io/";
+    //this._url="http://localhost:1337/";
+    this._url="https://examen-twj-mauriciocabrera-mauseb20.c9users.io/";
   }
   get url ():string{
     return this._url;
