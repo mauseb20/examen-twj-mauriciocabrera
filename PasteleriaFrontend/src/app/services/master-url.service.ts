f6bf509ccb543760b41ca3b8b5277c6b52a02da5
Mauricio Cabrera <mauseb91@gmail.com>
2017-03-07 03:26:35 -0500

    commit final

--- PasteleriaFrontend/src/app/services/master-url.service.ts
+++ PasteleriaFrontend/src/app/services/master-url.service.ts
@@ -6,6 +6,7 @@ export class MasterUrlService {
   private _url:string;
   constructor() {
     this._url="http://localhost:1337/";
+    //this._url="https://examen-twj-mauriciocabrera-mauseb20.c9users.io/";
   }
   get url ():string{
     return this._url;
